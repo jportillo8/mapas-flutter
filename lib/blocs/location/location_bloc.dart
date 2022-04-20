ff876bf806c11e02d886e48d05585de87f5b0d84
jportillo8 <buttokk@gmail.com>
2022-04-20 11:11:18 -0500

    Implementado seguimiento del usuario y historial

--- lib/blocs/location/location_bloc.dart
+++ lib/blocs/location/location_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 part 'location_event.dart';
 part 'location_state.dart';
@@ -11,18 +12,24 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
   StreamSubscription<Position>? positionStream;
 
   LocationBloc() : super(const LocationState()) {
-    on<LocationEvent>((event, emit) {});
+    on<OnNewUserLocationEvent>((event, emit) {
+      emit(state.copyWith(
+        lastKnowLocation: event.newLocation,
+        // Esparce todo lo que viene en el arreglo y añade el ultimo
+        myLocationHistory: [...state.myLocationHistory, event.newLocation],
+      ));
+    });
   }
 
   Future getCurrentPosition() async {
     /*Esto me determina la posicion actual de el user*/
     final position = await Geolocator.getCurrentPosition();
-    print('Position: $position');
-    // TODO retornar un objeto de tipo LatLng
+    // print('Position: $position');
+    add(OnNewUserLocationEvent(LatLng(position.latitude, position.longitude)));
   }
 
   void starFollowingUser() {
-    print('starFollowingUser');
+    // print('starFollowingUser');
     /*Se activa cada vez que la ubicación cambia dentro
      de los límites de [LocationSettings.accuracy] proporcionado.
      Este evento inicia todos los sensores de ubicación en el 
@@ -30,15 +37,21 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
      escucha de la transmisión o cuando se elimine la aplicación.*/
     positionStream = Geolocator.getPositionStream().listen((event) {
       final position = event;
-      print('Posicion: $position');
+      add(OnNewUserLocationEvent(
+          LatLng(position.latitude, position.longitude)));
+      // print('Posicion: $position');
     });
   }
 
+  /*Esta funcion me permite cerrar el Stream para no tener
+  fugas de memoria*/
   void stopFollowingUser() {
     positionStream?.cancel();
     print('stopFollowingUser');
   }
 
+  /*Esta funcion me permite cerrar el Stream para no tener
+  fugas de memoria*/
   @override
   Future<void> close() {
     starFollowingUser();
--- lib/blocs/location/location_state.dart
+++ lib/blocs/location/location_state.dart
@@ -2,12 +2,29 @@ part of 'location_bloc.dart';
 
 class LocationState extends Equatable {
   final bool followingUser;
+  /*Ultima ubicacion que conozco de el usuario*/
+  final LatLng? lastKnowLocation;
+  final List<LatLng> myLocationHistory;
   // TODO
   // ultimo geolocation
   // historia
 
-  const LocationState({this.followingUser = false});
+  const LocationState(
+      {this.followingUser = false, this.lastKnowLocation, myLocationHistory})
+      : myLocationHistory = myLocationHistory ?? const [];
+
+  LocationState copyWith({
+    bool? followingUser,
+    LatLng? lastKnowLocation,
+    List<LatLng>? myLocationHistory,
+  }) =>
+      LocationState(
+        followingUser: followingUser ?? this.followingUser,
+        lastKnowLocation: lastKnowLocation ?? this.lastKnowLocation,
+        myLocationHistory: myLocationHistory ?? this.myLocationHistory,
+      );
 
   @override
-  List<Object> get props => [followingUser];
+  List<Object?> get props =>
+      [followingUser, lastKnowLocation, myLocationHistory];
 }
