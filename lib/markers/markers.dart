d935f31dffb617b5edf8287fbef00ced7d66e33e
jportillo8 <buttokk@gmail.com>
2022-05-02 15:16:35 -0500

    Creacion de marcadores con custom painter

---
+++ lib/markers/markers.dart
@@ -0,0 +1,2 @@
+export 'package:maps_flutter_x0/markers/start_marker.dart';
+export 'package:maps_flutter_x0/markers/end_marker.dart';
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,3 +1,4 @@
 export 'package:maps_flutter_x0/screens/gps_access_screen.dart';
 export 'package:maps_flutter_x0/screens/map_screen.dart';
 export 'package:maps_flutter_x0/screens/loading_screen.dart';
+export 'package:maps_flutter_x0/screens/test_marker.dart';
